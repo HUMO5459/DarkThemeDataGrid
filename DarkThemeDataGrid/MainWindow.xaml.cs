8e9018d52d3cfe72186a0f2c2d672cfd78081f54
Humoyun Ochilov Ravshan o'g'li <43071439+HUMO5459@users.noreply.github.com>
2025-05-07 21:20:13 +0500

    Add project files.

---
+++ DarkThemeDataGrid/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using DarkThemeDataGrid.Models;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DarkThemeDataGrid;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public ObservableCollection<Order> Orders { get; set; }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        Orders = GenerateRandomOrders(20); // 20 ta random data
+        dataGrid.ItemsSource = Orders;
+    }
+
+    private ObservableCollection<Order> GenerateRandomOrders(int count)
+    {
+        var random = new Random();
+        var customerNames = new[] { "Franklin", "Martin", "George", "Zachary", "James", "Thomas", "Rutherford", "Abraham", "Grover" };
+        var countries = new[] { "Argentina", "Brazil", "USA", "Germany", "Japan", "Canada", "UK" };
+        var imageFiles = new[]
+        {
+        "Images/img1.png",
+        "Images/img2.png",
+        "Images/img3.png",
+        "pImages/img4.png",
+        "Images/img5.png"
+    };
+
+        var orders = new ObservableCollection<Order>();
+
+        for (int i = 0; i < count; i++)
+        {
+            orders.Add(new Order
+            {
+                OrderId = 10000 + i,
+                CustomerId = 11000 + i,
+                CustomerName = customerNames[random.Next(customerNames.Length)],
+                ShipCountry = countries[random.Next(countries.Length)],
+                ShippingDate = RandomDate(random, new DateTime(2000, 1, 1), DateTime.Now),
+                DeliveryStatus = random.Next(2) == 0,
+                ImagePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, imageFiles[random.Next(imageFiles.Length)])  // 🖼 Rasm yo‘li
+            });
+        }
+
+        return orders;
+    }
+
+
+    private DateTime RandomDate(Random random, DateTime start, DateTime end)
+    {
+        var range = (end - start).Days;
+        return start.AddDays(random.Next(range));
+    }
+
+    private void EditButton_Click(object sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        var order = button?.DataContext as Order;
+        if (order != null)
+        {
+            MessageBox.Show($"Editing Order ID: {order.OrderId}\nCustomer: {order.CustomerName}", "Edit Order");
+        }
+    }
+
+    private void Image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+        var image = sender as Image;
+        if (image?.DataContext is Order order && !string.IsNullOrEmpty(order.ImagePath))
+        {
+            var viewer = new ImageViewerWindow(order.ImagePath);
+            viewer.Owner = this; // Optional: parent window
+            viewer.ShowDialog(); // Modal
+        }
+    }
+}
---
+++ DarkThemeDataGrid/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarkThemeDataGrid.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string ShipCountry { get; set; }
+        public DateTime ShippingDate { get; set; }
+        public bool DeliveryStatus { get; set; }
+        public string ImagePath { get; set; }
+    }
+
+}
